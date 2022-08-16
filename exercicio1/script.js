e8cb112a67dcb3587305cc91c932dc3483fb874c
Hugowave88 <hugohedgehog24@gmail.com>
2022-08-16 18:39:00 -0300

    Laços

---
+++ exercicio1/script.js
@@ -0,0 +1,7 @@
+const coxinha = prompt('Mais coxinhas ?')
+let conta = 2.50
+
+while (coxinha !=='s') {
+    
+    
+}
---
+++ exercicio2/script.js
@@ -0,0 +1,7 @@
+let tabuada = 0
+console.log('Tabuada de 7: ')
+for (let i = 0; i <=10; i++){
+    tabuada =7*i
+    console.log(`7x${i} = ${tabuada}`);
+}
+console.log('')
